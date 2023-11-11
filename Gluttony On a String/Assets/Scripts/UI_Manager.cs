0a7261d210b7c26e74fbe81e4942e6e3bc9dfadb
Bommar257 <j2bomm@gmail.com>
2023-11-11 00:18:02 -0600

    Enemy Objects

---
+++ Gluttony On a String/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        scoreText.text = "Score: " + 0;
+        */
+    }
+    /*
+    public void updateScore(int playerScore)
+    {
+        scoretext.text ="Score: " + playerScore.ToString();
+    }*/
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
